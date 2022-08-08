bd79df09f7f9063f473d582fce0df055bec40f52
Agustin Navarro Torres <agusnavarro11@gmail.com>
2022-08-08 17:29:33 +0200

    run.sh

---
+++ ChampSim/Berti/obj/src/uncore.d
@@ -0,0 +1,19 @@
+obj/src/uncore.o: src/uncore.cc inc/uncore.h inc/champsim.h inc/cache.h \
+ inc/memory_class.h inc/block.h inc/instruction.h inc/set.h \
+ inc/dram_controller.h
+
+inc/uncore.h:
+
+inc/champsim.h:
+
+inc/cache.h:
+
+inc/memory_class.h:
+
+inc/block.h:
+
+inc/instruction.h:
+
+inc/set.h:
+
+inc/dram_controller.h:
